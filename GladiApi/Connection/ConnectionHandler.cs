e85eee90cc9e943b39397ed698f190804ec09913
Daniel <49683346+0tii@users.noreply.github.com>
2022-08-28 20:06:08 +0200

    EOD commit

--- GladiApi/Connection/ConnectionHandler.cs
+++ GladiApi/Connection/ConnectionHandler.cs
@@ -4,7 +4,8 @@ namespace GladiApi.Connection
 {
     /// <summary>
     /// Handles connecting to the gladiatus gameforge server.<br/>
-    /// Returns a <see cref="CharacterSelector"/> of available characters for the user.
+    /// Returns a <see cref="CharacterSelector"/> of available characters for the user.<br/><br/>
+    /// <br/><b>Abandoned since authenticating against GF servers is secured by captcha</b>
     /// </summary>
     public class ConnectionManager
     {
